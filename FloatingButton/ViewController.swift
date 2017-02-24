2f84f6b0eb0e0bf0fbd8fdf7932a05de727f3044
Mayank Gupta <mayankIndia@mayanks-macbook-air.local>
2017-02-25 01:05:07 +0530

    Addded float action button pod

--- FloatingButton/ViewController.swift
+++ FloatingButton/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import KCFloatingActionButton
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController ,KCFloatingActionButtonDelegate{
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        layoutFAB()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func layoutFAB() {
+        let fab = KCFloatingActionButton()
+        let item = KCFloatingActionButtonItem()
+        item.buttonColor = UIColor(red: 188/255, green: 46/255, blue: 35/255, alpha: 1)
+        item.circleShadowColor = UIColor.red
+        item.titleShadowColor = UIColor(red: 188/255, green: 46/255, blue: 35/255, alpha: 0.5)
+        item.title = "Custom item"
+        item.handler = { item in
+            
+        }
+        
+        //fab.addItem(title: "I got a title")
+        
+        fab.addItem("Become an Admin", icon: UIImage(named: "Contacts Filled.png")) { item in
+            let alert = UIAlertController(title: "Become an Admin", message: "Send a request to be an admin and be able to write notices", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Send Request", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        //fab.addItem(item: item)
+        fab.fabDelegate = self
+        
+        self.view.addSubview(fab)
+        
     }
-
 
 }
 
